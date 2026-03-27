92f391f2fe4ef83115c0a4ed2baa43b05857ad5d
Noufan VP <vpnoufan33@gmail.com>
2026-03-27 17:06:28 +0530

    Neon postgres Database is added

--- ssp_website/settings.py
+++ ssp_website/settings.py
@@ -77,12 +77,22 @@ WSGI_APPLICATION = 'ssp_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    # Production: Use PostgreSQL (Neon)
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
     }
-}
+else:
+    # Development: Use SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
